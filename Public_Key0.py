4c1a25b66e941fa470c9239ce4c818e83020f5a3
fjaviergb <54247560+fjaviergb@users.noreply.github.com>
2019-08-19 10:31:06 +0200

    Delete Public_Key0.py

--- Public_Key0.py
+++
@@ -1,6 +0,0 @@
------BEGIN PUBLIC KEY----- 
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCzrvbnUmi5p9gEOq80eQKjbDwz 
-dp1EgUivg34dlgjcy9sMKxJKPT1ceZ2EcoFQHVSrsbRJ+yjhdVBkFdQc+C6QRTDz 
-PH5ptavO8yccSpVovJReROl/dw+ze+ddJusIGZWYXGeoqKb5FgUQ/a+mg02tlbgp 
-xHJsmmP24jPkovX++QIDAQAB
------END PUBLIC KEY-----
